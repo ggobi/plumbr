cc149d4b6e77cd028e4450760a743ce42ac11038
Hadley Wickham <hadley@rice.edu>
2010-06-03 15:46:10 +0000

    Notes on events

---
+++ R/events.r
@@ -0,0 +1,17 @@
+# Plumbr events
+#
+# Plumbr data structures send only single event for data changes:
+# data_changed.  This has a single argument which is either NULL, 
+# representing a change in the shape of the underlying data, or a two column
+# matrix giving the locations of changed data values.
+#
+
+
+
+#' @events a list of event parameters
+#' @returns a unified event
+combine_data_events <- function(events) {
+  if (any(sapply(events, is.null))) return(NULL)
+  
+  do.call("rbind", events)
+}
