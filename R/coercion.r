bc05b8ec235578fbf9d3736711c1c32c7fd8bd8e
Michael Lawrence <lawremi@gmail.com>
2011-08-03 20:32:41 -0700

    Fix coercion to data frame in zero column case

--- R/coercion.r
+++ R/coercion.r
@@ -12,7 +12,14 @@ as.mutaframe.default <- function(x, ...) as.mutaframe(as.data.frame(x, ...))
 as.data.frame.mutaframe <- function(x, row.names=rownames(x), optional=FALSE, ...) {
   cols <- lapply(names(x), function(j) x[[j]])
   names(cols) <- names(x)
-  as.data.frame(cols, row.names = row.names, optional = optional, ...)
+  df <- as.data.frame(cols, optional = optional, ...)
+  ## we set row.names this way for cases where we have no columns, but >0 rows
+  ## as.data.frame complains in that case
+  attr(df, "row.names") <- row.names
+  df
 }
 
 is.mutaframe <- function(x) inherits(x, "mutaframe")
+
+## as.list.environment does not resolve active bindings
+as.list.mutaframe <- function(x) lapply(x, do.call, list())
